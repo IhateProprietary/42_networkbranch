eb60acf2ea7cf58cd61e63edfbfa1024d1cb21a0
IhateProprietary <death2511@hotmail.com>
2018-03-05 02:17:34 +0100

    change ts3 patch to local

--- docker-1/01_dockerfiles/ex01/launch_teamspeak.sh
+++ docker-1/01_dockerfiles/ex01/launch_teamspeak.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# docker build -t ts3:latest .
-docker run --name teamspeak -p 9987:9987/udp -p 10011:10011 -p 30033:30033 -d ts3:latest
+# docker build -t ts3:local
+docker run --name ts3 -p 9987:9987/udp -p 10011:10011 -p 30033:30033 -d ts3:local
